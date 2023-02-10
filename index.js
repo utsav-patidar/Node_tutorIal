88c2d5f931ff7a7072aa60edf9335332209d4368
utsav-patidar <patelutsav22082003@gmail.com>
2023-02-10 12:13:36 +0530

    commit

--- index.js
+++ index.js
@@ -9,4 +9,3 @@ http.createServer((req,resp)=>{
      resp.write("<h2>Hello world i m utsav</h2>");//it is used to write something on server
      resp.end();// it must always end the resp end
 }).listen(4000);// in which port you run your server
-console.log("Hello");
